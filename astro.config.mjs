f7348fe65aed0a1c1001d3abe477e1e3bc280da9
SuliannBenHamed <95860756+SuliannBenHamed@users.noreply.github.com>
2025-12-05 10:20:11 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import github from "@astrojs/github";
 
 export default defineConfig({
   site: "https://observatoire-o2er.github.io/o2er",
-  base: "/o2er",
-  integrations: [github()],
+  base: "/o2er"
 });
