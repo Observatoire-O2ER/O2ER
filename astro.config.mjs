0cc9a48989c19e0052c37232884dc49d0f2fda48
SuliannBenHamed <95860756+SuliannBenHamed@users.noreply.github.com>
2026-02-28 15:31:02 +0100

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://observatoire-o2er.github.io/o2er",
-  base: "/O2ER/"
+  site: "https://observatoire-o2er.github.io",
+  base: "/O2ER",
 });
